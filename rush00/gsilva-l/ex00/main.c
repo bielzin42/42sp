7b8d1bdeddb6c7ff5a1c190dba223c0ddaf2734a
Gabriel Lima <nickspoot@gmail.com>
2020-02-22 11:16:40 -0300

    42CPiscineSP exercises

---
+++ rush00/gsilva-l/ex00/main.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnascime <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/23 11:08:04 by dnascime          #+#    #+#             */
+/*   Updated: 2019/11/25 13:47:59 by gsilva-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void		rush(int x, int y);//prototipa que em algum momento vamos usar essa função
+
+int			main(void)//então começamos a nossa função principal
+{
+	rush(5, 5);//aqui, declaramos os valores das variáveis dos parâmetros da função rush
+	return (0);// return zero por que não queremos nenhuma resposta no imput padrão
+}
